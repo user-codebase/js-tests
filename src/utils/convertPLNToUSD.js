2ed4260022b732f855f062a056d7d9916487c92a
Marcin Suder <marcin.code.github@gmail.com>
2026-03-08 03:41:43 +0100

    add first test

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,19 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (PLN === undefined) return NaN;
+
+  if (typeof PLN === 'string') return NaN;
+
+  if (typeof PLN !== 'number') return 'Error';
+
+  if (PLN < 0) return '$0.00';
+
+  const PLNtoUSD = PLN / 3.5;
+  
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
